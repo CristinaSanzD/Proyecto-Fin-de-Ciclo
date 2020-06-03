4acdd69774e184b9c5828fe86814f70fbcf570b9
Cristina <criscantalejo@hotmail.com>
2020-06-03 11:16:41 +0200

    ProyectoFC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,7 +67,6 @@ import { MisPedidosComponent } from './paginas/mis-pedidos/mis-pedidos.component
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     ReactiveFormsModule,
     AngularFireStorageModule
